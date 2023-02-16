aa9484aa61e0cc6503ce85c087b88e6cac8e4109
Umut Kızıloğlu <hello@umutk.me>
2023-02-16 15:55:47 +0000

    order mongodb schema created

---
+++ schemas/order.scheme.ts
@@ -0,0 +1,24 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument, Types } from 'mongoose';
+
+export type OrderDocument = HydratedDocument<Order>;
+
+@Schema()
+export class Order {
+  @Prop()
+  order_id: number;
+
+  @Prop({ type: Types.ObjectId, ref: 'product' })
+  product_id: number;
+
+  //   @Prop({ type: Types.ObjectId, ref: 'user_id' })
+  //   user_id: number;
+
+  @Prop()
+  created_at: Date;
+
+  @Prop()
+  updated_at: Date;
+}
+
+export const OrderSchema = SchemaFactory.createForClass(Order);
