fa69943170fc29c7f7879d73e26c7541d740ddc9
Umut Kızıloğlu <hello@umutk.me>
2023-02-16 15:58:43 +0000

    create order service added

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { AppService } from './app.service';
+import { CreateOrderDataRequest } from 'dto/order.dto';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post('createOrder')
+  createOrder(
+    @Body() { product_id, stock_quantity }: CreateOrderDataRequest,
+  ): string {
+    return this.appService.createOrder({ product_id, stock_quantity });
+  }
+
+  @Get('getCampaigns')
+  getCampaigns(): string {
+    return this.appService.getCampaigns();
+  }
+
+  @Get('getOrderOne')
+  getOrderOne(): string {
+    return this.appService.getOrderOne();
   }
 }
