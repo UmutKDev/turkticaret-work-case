4a5b74b8b39251d2f5c17e3c395f05b03af4ddc4
Umut Kızıloğlu <hello@umutk.me>
2023-02-17 00:47:01 +0000

    Campaign schema connected to mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Product, ProductSchema } from 'schemas/product.schema';
 import { Order, OrderSchema } from 'schemas/order.scheme';
+import { Campaign, CampaignSchema } from 'schemas/campaign.scheme';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { Order, OrderSchema } from 'schemas/order.scheme';
     MongooseModule.forFeature([
       { name: Product.name, schema: ProductSchema },
       { name: Order.name, schema: OrderSchema },
+      { name: Campaign.name, schema: CampaignSchema },
     ]),
   ],
   controllers: [AppController],
