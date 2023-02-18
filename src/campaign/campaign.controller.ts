27cee1f37d9cffb6f2e9d6ea3bd9217ca5832e9e
Umut Kızıloğlu <hello@umutk.me>
2023-02-18 14:04:13 +0000

    campaign controller created

---
+++ src/campaign/campaign.controller.ts
@@ -0,0 +1,40 @@
+import { Controller, Get, Post, Body, Query } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { CampaignService } from './campaign.service';
+import { Campaign } from 'schemas/campaign.schema';
+import { CreateCampaignDataRequest } from 'src/campaign/dto/campaign.dto';
+
+@Controller('campaign')
+@ApiTags('Campaign API Endpoints')
+export class CampaignController {
+  constructor(private readonly campaignService: CampaignService) {}
+
+  @Post('create')
+  async createCampaign(
+    @Body()
+    {
+      campaign_id,
+      campaign_name,
+      discount_rate,
+      max_product_count,
+      author_name,
+      category_id,
+      min_cost,
+    }: CreateCampaignDataRequest,
+  ): Promise<Campaign> {
+    return this.campaignService.createCampaign({
+      campaign_id,
+      campaign_name,
+      discount_rate,
+      max_product_count,
+      author_name,
+      category_id,
+      min_cost,
+    });
+  }
+
+  @Get('all')
+  getCampaigns(): string {
+    return this.campaignService.getCampaigns();
+  }
+}
