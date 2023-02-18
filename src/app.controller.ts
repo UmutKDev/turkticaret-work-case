e590125777b5370c250d7df80259e664268aa8f3
Umut Kızıloğlu <hello@umutk.me>
2023-02-18 13:48:44 +0000

    app controller updated

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,45 +1,11 @@
-import { Controller, Get, Post, Body, Query } from '@nestjs/common';
-import { AppService } from './app.service';
-import { CreateOrderDataRequest, GetOrderDataRequest } from 'dto/order.dto';
-import { Order } from 'schemas/order.schema';
-import { Campaign } from 'schemas/campaign.schema';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Post('createOrder')
-  async createOrder(
-    @Body()
-    { products, campaign_id }: CreateOrderDataRequest,
-  ): Promise<Order> {
-    return this.appService.createOrder({
-      products,
-      campaign_id,
-    });
-  }
-
-  @Get('getCampaigns')
-  getCampaigns(): string {
-    return this.appService.getCampaigns();
-  }
-
-  @Post('createCampaign')
-  async createCampaign(
-    @Body() { campaign_id, campaign_name, discount_rate, max_product_count },
-  ): Promise<Campaign> {
-    return this.appService.createCampaign({
-      campaign_id,
-      campaign_name,
-      discount_rate,
-      max_product_count,
-    });
-  }
-
-  @Get('getOrderOne')
-  async getOrderOne(
-    @Query() { order_id }: GetOrderDataRequest,
-  ): Promise<Order | Campaign> {
-    return this.appService.getOrderOne({ order_id });
+  @Get('/')
+  getHello(): object {
+    return {
+      message: 'Hello World!',
+    };
   }
 }
