9682357bdc343293251a334a1aaf558e8027b892
Umut Kızıloğlu <hello@umutk.me>
2023-02-18 14:02:54 +0000

    order dto updated

---
+++ src/order/dto/order.dto.ts
@@ -0,0 +1,32 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsNumber, IsArray } from 'class-validator';
+
+export class CreateOrderDataRequest {
+  @ApiProperty({
+    type: [Object],
+    isArray: true,
+    allOf: [
+      {
+        type: 'object',
+        properties: {
+          id: { type: 'number' },
+          quantity: { type: 'number' },
+        },
+      },
+    ],
+  })
+  @IsNotEmpty()
+  @IsArray()
+  products: Array<{ id: number; quantity: number }>;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsNumber()
+  campaign_id: number;
+}
+
+export class GetOrderDataRequest {
+  @ApiProperty()
+  @IsNotEmpty()
+  order_id: string;
+}
