7a8c4941cbea75e57d95776737daf81d19dd4267
Umut Kızıloğlu <hello@umutk.me>
2023-02-18 14:04:39 +0000

    campaign service created

---
+++ src/campaign/campaign.service.ts
@@ -0,0 +1,32 @@
+import { Injectable, HttpException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Campaign } from 'schemas/campaign.schema';
+
+@Injectable()
+export class CampaignService {
+  constructor(@InjectModel(Campaign.name) private campaignModel) {}
+
+  getCampaigns(): string {
+    return 'This action returns all campaigns';
+  }
+
+  async createCampaign({
+    campaign_id,
+    campaign_name,
+    discount_rate,
+    max_product_count,
+    author_name,
+    category_id,
+    min_cost,
+  }): Promise<Campaign> {
+    return this.campaignModel.create({
+      campaign_id: 1,
+      campaign_name:
+        "Sabahattin Ali'nin Roman kitaplarında 2 üründen 1 tanesi bedava.",
+      discount_rate: 50,
+      max_product_count: 2,
+      author_name: 'Sabahattin Ali',
+      category_title: 'Roman',
+    });
+  }
+}
