4745d7da409022ae4a76da6cdfa59a3e829acf84
Umut Kızıloğlu <hello@umutk.me>
2023-02-23 22:45:36 +0000

    campaign apply and stock available fixed

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { Injectable, HttpException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Order } from 'schemas/order.schema';
@@ -16,88 +17,102 @@ export class OrderService {
   ) {}
 
   async createOrder({ products }): Promise<OrderResponse> {
-    const productIds = products.map((product) => product.id);
-    const productQuantities = products.map((product) => product.quantity);
-    const productStocks = await this.productModel
-      .find({ product_id: { $in: productIds } })
-      .select('stock_quantity product_id -_id list_price author category_id')
+    const campaigns = await this.campaignModel.find().lean();
+
+    if(products.length === 0) {
+      throw new HttpException('Product list is empty', 400);
+    }
+    
+    const productIds = products.map((product) => ({
+      id: product.id,
+      quantity: product.quantity,
+    }));
+    const productItems = await this.productModel
+      .find({ product_id: { $in: productIds.map((x) => x.id) } })
+      .select(
+        'stock_quantity product_id -_id list_price author_name category_id',
+      )
       .lean();
 
-    const isStockAvailable = productStocks.every(
-      (productStock, index) =>
-        productStock.stock_quantity >= productQuantities[index],
-    );
+     productIds.map((id) => {
+      productItems
+        .filter((f) => f.product_id === id.id)
+        .map((w) => {
+          if (w.stock_quantity >= id.quantity) {
+            return true;
+          } else {
+            throw new HttpException(
+              `Product IDs: ${w.product_id} is not available in stock`,
+              400,
+            );
+          }
+        });
+    });
 
-    if (!isStockAvailable) {
-      const notAvailableProduct = productStocks.find(
-        (productStock, index) =>
-          productStock.stock_quantity < productQuantities[index],
-      );
-
-      throw new HttpException(
-        `Product IDs: ${notAvailableProduct.product_id} is not available in stock`,
-        400,
-      );
-    }
-
-    let totalPrice = productStocks.reduce(
+    let totalPrice = productItems.reduce(
       (total, productStock, index) =>
-        total + productStock.list_price * productQuantities[index],
+        total + productStock.list_price * productIds[index].quantity,
       0,
     );
 
     let discountPrice = 0;
 
-    const campaigns = await this.campaignModel.find().lean();
-
-    const campaignauthorname = productStocks.filter((item) =>
+    const campaignauthorname = productItems.filter((item) =>
       campaigns.some(
         (campaign) =>
-          campaign.author_name === item.author &&
+          campaign.author_name === item.author_name &&
           item.category_id === campaign.category_id,
       ),
     );
 
-    const foundCampaignData = await this.campaignModel.findOne({
-      author_name: campaignauthorname[0].author,
-      category_id: campaignauthorname[0].category_id,
-    });
-
-    const discountRate = foundCampaignData.discount_rate;
-    const minOrderAmount = foundCampaignData.min_order_amount;
-    const minProductCount = foundCampaignData.min_product_count;
-
     let campaignItemQuantity = 0;
 
     products.filter((f) =>
-      productStocks
+      productItems
         .filter(
           (pS) =>
             pS.product_id === f.id &&
-            pS.author === campaignauthorname[0].author,
+            pS.author_name === campaignauthorname[0]?.author_name,
         )
         .map(() => (campaignItemQuantity += f.quantity)),
     );
+    const authorCamp = campaigns.filter((cF) => cF.author_name && cF.category_id);
+    const percentCamp = campaigns.filter((cF) => cF.author_name && cF.category_id);
 
-    if (campaignItemQuantity >= minProductCount) {
-      productStocks.map((item) => (totalPrice += item.list_price));
+    let activeCamp = {} as any
+
+    if (campaignItemQuantity >= authorCamp[0].min_product_count) {
+      productItems.map((item) => (totalPrice += item.list_price));
       const topPrice = Math.max(...campaignauthorname.map((o) => o.list_price));
-      if (Number(totalPrice) > minOrderAmount) {
-        if (
-          totalPrice - topPrice <
-          totalPrice - (totalPrice / 100) * discountRate
-        ) {
+      if (totalPrice > percentCamp[0].min_order_amount) {
+        if (totalPrice - topPrice < totalPrice - (totalPrice / 100) * percentCamp[0].discount_rate) {
+          activeCamp = {
+            id:  authorCamp[0].campaign_id,
+            name:  authorCamp[0].campaign_name
+          }
           discountPrice = totalPrice - topPrice;
         } else {
-          discountPrice = totalPrice - totalPrice / (100 * discountRate);
+          activeCamp = {
+            id:  authorCamp[0].campaign_id,
+            name:  authorCamp[0].campaign_name
+          }          
+          discountPrice = totalPrice - totalPrice / (100 * percentCamp[0].discount_rate);
         }
       } else {
+        activeCamp = {
+          id:  authorCamp[0].campaign_id,
+          name:  authorCamp[0].campaign_name
+        }        
         discountPrice = totalPrice - topPrice;
       }
     } else {
-      productStocks.map((item) => (totalPrice += item.list_price));
-      if (Number(totalPrice) > minOrderAmount) {
-        discountPrice = totalPrice - totalPrice / (100 * discountRate);
+      productItems.map((item) => (totalPrice += item.list_price));
+      if (Number(totalPrice) > percentCamp[0].min_order_amount) {
+        activeCamp = {
+          id:  authorCamp[0].campaign_id,
+          name:  authorCamp[0].campaign_name
+        }        
+        discountPrice = totalPrice - totalPrice / (100 * percentCamp[0].discount_rate);
       } else {
         discountPrice = 0;
       }
@@ -107,33 +122,30 @@ export class OrderService {
 
     const order = await this.orderModel.create({
       order_id: Math.floor(Math.random() * 1000000),
-      products: productStocks.map((productStock) => productStock.product_id),
-      campaign_id: foundCampaignData ? foundCampaignData.campaign_id : null,
+      products: productItems.map((productStock) => productStock.product_id),
+      campaign_id: activeCamp ? activeCamp.id : null,
       amount: {
         total: totalPrice,
         totalWithDiscount: discountPrice,
-        discount: foundCampaignData ? foundCampaignData.discount_rate : 0,
         shippment: isShippmentDiscountAvailable ? 0 : 35,
       },
     });
 
     return {
       order_id: order.order_id,
-      products: productStocks.map((productStock) => ({
+      products: productItems.map((productStock) => ({
         id: productStock.product_id,
         title: productStock.title,
         list_price: productStock.list_price,
         stock_quantity: productStock.stock_quantity,
       })),
       campaign: {
-        id: foundCampaignData ? foundCampaignData.campaign_id : null,
-        name: foundCampaignData ? foundCampaignData.campaign_name : null,
-        discount_rate: foundCampaignData ? foundCampaignData.discount_rate : 0,
+        id: activeCamp ? activeCamp.id : null,
+        name: activeCamp ? activeCamp.name : null
       },
       amount: {
         total: order.amount.total,
         totalWithDiscount: order.amount.totalWithDiscount,
-        discount: order.amount.discount,
         shippment: order.amount.shippment,
       },
       date: order.date,
@@ -167,12 +179,10 @@ export class OrderService {
       campaign: {
         id: campaign.campaign_id,
         name: campaign.campaign_name,
-        discount_rate: campaign.discount_rate,
       },
       amount: {
         total: order.amount.total,
         totalWithDiscount: order.amount.totalWithDiscount,
-        discount: order.amount.discount,
         shippment: order.amount.shippment,
       },
       date: order.date,
